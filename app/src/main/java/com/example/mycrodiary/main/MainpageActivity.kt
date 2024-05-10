0ffcb5c38e251604c548cbfc8cc4cbd8641a70a3
alsgudans <painapple57@gmail.com>
2024-05-11 04:39:54 +0900

    fixed error

--- app/src/main/java/com/example/mycrodiary/main/MainpageActivity.kt
+++ app/src/main/java/com/example/mycrodiary/main/MainpageActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mycrodiary.databinding.ActivityMainpageBinding
-import com.example.mycrodiary.menu.MypageActivity
 
 class MainpageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +11,6 @@ class MainpageActivity : AppCompatActivity() {
         val binding = ActivityMainpageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val move_mypage = Intent(this,MypageActivity::class.java)
-
-        binding.myPage.setOnClickListener(){
-            startActivity(move_mypage)
-        }
 
     }
 }
