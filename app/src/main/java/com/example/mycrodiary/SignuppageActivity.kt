f9e65e61f60fc370dae7f64ad84ceade3a035140
alsgudans <painapple57@gmail.com>
2024-05-11 00:21:39 +0900

    done login page UI and login service

--- app/src/main/java/com/example/mycrodiary/SignuppageActivity.kt
+++ app/src/main/java/com/example/mycrodiary/SignuppageActivity.kt
@@ -3,12 +3,17 @@ package com.example.mycrodiary
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mycrodiary.databinding.ActivitySignuppageBinding
 
 class SignuppageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val binding = ActivitySignuppageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
