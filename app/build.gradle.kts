f03c53a2e5c0f01e9d8be95755e1fedd5252f02b
alsgudans <painapple57@gmail.com>
2024-05-10 22:59:49 +0900

    done login page UI and login service

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    viewBinding{
+        enable = true
+    }
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -40,6 +43,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
