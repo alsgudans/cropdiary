12886fde0d7bbfbff86fd21d2b8bb55e5f9877ca
alsgudans <painapple57@gmail.com>
2024-06-16 02:09:12 +0900

    나의 일지 수정

--- app/src/main/java/com/example/mycrodiary/Database_Utils/InputDataInfo.kt
+++ app/src/main/java/com/example/mycrodiary/Database_Utils/InputDataInfo.kt
@@ -2,6 +2,10 @@ package com.example.mycrodiary.Database_Utils
 
 data class InputDataInfo(
     val day: String? = null,
+    val group1: Int? = null,
+    val group2: Int? = null,
+    val group3: Int? = null,
+    val group4: Int? = null,
     val weight: Double? = 0.0,
     val humidity: Double? = 0.0,
     val illumination: Double? = 0.0,
